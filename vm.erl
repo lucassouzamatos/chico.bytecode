f280b19fc928d995a22a7dde31668160c9afeb78
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2022-11-11 08:43:37 -0300

    add binary operations

--- vm.erl
+++ vm.erl
@@ -36,6 +36,19 @@ run(#vm{chunk = Chunk} = Vm, Offset) ->
 run_instruction({op, return}, Vm, Offset) ->
   {Value, UpdatedVm} = update_vm_stack({pop}, Vm),
   {ok, Value, UpdatedVm, Offset + 1};
+run_instruction({op, negate}, Vm, Offset) ->
+  {{value, Value}, Vm0} = update_vm_stack({pop}, Vm),
+  Vm1 = update_vm_stack({push, {value, -Value}}, Vm0),
+  {ok, Vm1, Offset + 1};
+run_instruction({op, positive}, Vm, Offset) ->
+  {{value, Value}, Vm0} = update_vm_stack({pop}, Vm),
+  Vm1 = update_vm_stack({push, {value, +Value}}, Vm0),
+  {ok, Vm1, Offset + 1};
+run_instruction({op, binary, Op}, Vm, Offset) ->
+  {{value, Value0}, Vm0} = update_vm_stack({pop}, Vm),
+  {{value, Value1}, Vm1} = update_vm_stack({pop}, Vm0),
+  Vm2 = update_vm_stack({push, {value, erlang:(Op)(Value0, Value1)}}, Vm1),
+  {ok, Vm2, Offset + 1};
 run_instruction({op, constant}, Vm, Offset) ->
   #vm{ chunk = Chunk } = Vm,
   Value = chunk:get_instruction(Chunk, Offset + 1),
@@ -48,7 +61,9 @@ interpret(Chunk) ->
 test() -> 
   Chunk = chunk:start_chunk(),
   Chunk1 = Chunk#chunk{instructions = [
-    {op, constant}, {value, 1}, 
+    {op, constant}, {value, 1}, {op, negate},
+    {op, constant}, {value, 2}, {op, positive},
+    {op, binary, '+'},
     {op, return}
   ]},
   interpret(Chunk1).
